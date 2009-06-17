e1b418779823e5cb022a1eb88ad12a65475a0753
Arnaud TROEL <arnaud.troel@gmail.com>
2009-06-17 23:15:32 +0200

    b6_splay: added splay tree documentation

--- include/b6/refs.h
+++ include/b6/refs.h
@@ -79,7 +79,7 @@ struct b6_sref {
 };
 
 /**
- * @ingroup list
+ * @ingroup list, splay
  * @brief Double reference
  */
 struct b6_dref {
@@ -703,13 +703,48 @@ static inline struct b6_dref *b6_list_last(const struct b6_list *list)
 	return list->tail.ref[B6_PREV];
 }
 
+/**
+ * @defgroup splay Splay trees
+ *
+ * Splay trees are self-balanced binary search trees where recently accessed
+ * elements are naturally moved near the root. As such, there is no need of an
+ * extra balance parameter in reference. Thus, they have a small footprint in
+ * memory compared to other trees since they only make use of double
+ * references.
+ *
+ * The drawback is that in-order traversal is slower with splay trees than
+ * with AVL or red/black trees as moving from one reference to the next one
+ * sometimes require to find its parent, starting from the root.
+ */
+
+/**
+ * @ingroup splay
+ * @brief splay tree
+ */
 struct b6_splay {
-	struct b6_dref head, tail, *root;
-	b6_ref_compare_t comp;
+	struct b6_dref head; /**< reference before any element in the tree */
+	struct b6_dref tail; /**< reference after any element in the tree */
+	struct b6_dref *root; /**< reference that has no parent in the tree */
+	b6_ref_compare_t comp; /**< how elements compare to each other */
 };
 
+/**
+ * @ingroup splay
+ * @brief Initialize or clear a splay tree
+ * @complexity O(1)
+ * @param splay pointer to the splay tree
+ * @param compare pointer to the function to call for comparing elements
+ */
 void b6_splay_initialize(struct b6_splay *splay, b6_ref_compare_t compare);
 
+/**
+ * @ingroup splay
+ * @brief Test if a splay tree contains elements
+ * @complexity O(1)
+ * @param splay pointer to the splay tree
+ * @return 0 if the list contains one element or more and another value if it
+ * does not contains any elements
+ */
 static inline int b6_splay_empty(const struct b6_splay *splay)
 {
 	b6_precond(splay != NULL);
@@ -718,18 +753,78 @@ static inline int b6_splay_empty(const struct b6_splay *splay)
 	       &splay->head == splay->tail.ref[B6_PREV];
 }
 
-struct b6_dref *b6_splay_add(struct b6_splay *splay, struct b6_dref *ref);
+/**
+ * @ingroup splay
+ * @brief Insert a new element in the a splay tree
+ * @complexity O(log(n))
+ * @param splay pointer to the splay tree
+ * @param dref reference of the element to insert
+ * @return dref if insertion succeded or the pointer to the element in the
+ * tree that equals dref.
+ */
+struct b6_dref *b6_splay_add(struct b6_splay *splay, struct b6_dref *dref);
+
+/**
+ * @ingroup splay
+ * @brief Remove the element at the root of the splay tree
+ * @complexity O(log(n))
+ * @param splay pointer to the splay tree
+ * @return pointer to the reference of the element removed
+ */
 struct b6_dref *b6_splay_del(struct b6_splay *splay);
+
+/**
+ * @ingroup splay
+ * @brief Search a splay tree
+ * @param splay pointer to the splay tree
+ * @param examine pointer to the function to call when evaluation the elements
+ * or NULL for using the default comparison
+ * @param argument opaque data to pass to examine if not NULL or a pointer to
+ * the reference of an element to find an equal in splay.
+ * @return pointer to the element found that has become the root of the splay
+ * tree or NULL if no element was found.
+ */
 struct b6_dref *b6_splay_find(const struct b6_splay *splay,
                               b6_ref_examine_t examine, void *argument);
-struct b6_dref *b6_splay_walk(const struct b6_splay *splay,
-                              const struct b6_dref *ref, int direction);
 
+/**
+ * @ingroup splay
+ * @brief In-order traveling of a splay tree
+ * @complexity O(ln(n))
+ * @param splay pointer to the splay tree
+ * @param dref reference to walk from
+ * @param direction B6_PREV (B6_NEXT) to get the reference of the greatest
+ * smaller elements (smallest greater elements respectively)
+ * @return NULL when going out of range or the next or previous reference in
+ * the sequence
+ */
+struct b6_dref *b6_splay_walk(const struct b6_splay *splay,
+                              const struct b6_dref *dref, int direction);
+
+/**
+ * @ingroup splay
+ * @brief Return the reference of the smallest element in the splay tree
+ * according to how elements compare within it
+ * @complexity O(1)
+ * @param splay pointer to the splay tree
+ * @return pointer to the reference of the smallest element or tail if the
+ * tree is empty
+ */
 static inline struct b6_dref *b6_splay_first(const struct b6_splay *splay)
 {
 	return b6_splay_walk(splay, &splay->head, B6_NEXT);
 }
 
+/**
+ * @ingroup splay
+ * @brief Return the reference of the greatest element in the splay tree
+ * according to how elements compare within it
+ * @complexity O(1)
+ * @param deque pointer to the doubly-ended queue
+ * @param splay pointer to the splay tree
+ * @return pointer to the reference of the greatest element or tail if the
+ * tree is empty
+ */
 static inline struct b6_dref *b6_splay_last(const struct b6_splay *splay)
 {
 	return b6_splay_walk(splay, &splay->tail, B6_PREV);
