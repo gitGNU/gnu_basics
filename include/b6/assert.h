c0dd1cdd5aa7d8bfa9f4c05934b093d2fee8122a
Arnaud Troël <arnaud.troel@gmail.com>
2012-08-15 22:48:50 +0200

    assert: added b6_check and inhibited other when NDEBUG is defined.

--- include/b6/assert.h
+++ include/b6/assert.h
@@ -11,6 +11,12 @@
 extern void b6_assert_handler(const char *func, const char *file, int line,
                               int type, const char *condition);
 
+#define __B6_CHECK(type, condition)					\
+	do if (b6_unlikely(!(condition)))				\
+		b6_assert_handler(__func__, __FILE__, __LINE__, type,	\
+		                  #condition);				\
+	while(0)
+
 #define __B6_ASSERT(type, condition)					\
 	do if (b6_unlikely(!(condition)))				\
 		b6_assert_handler(__func__, __FILE__, __LINE__, type,	\
@@ -25,10 +31,17 @@ extern void b6_assert_handler(const char *func, const char *file, int line,
 		break;							\
 	} while (0)
 
-enum { B6_ASSERT, B6_PRECOND, B6_POSTCOND };
+enum { B6_CHECK, B6_ASSERT, B6_PRECOND, B6_POSTCOND };
 
-#define b6_assert(condition)	__B6_ASSERT(B6_ASSERT, condition)
-#define b6_precond(condition)	__B6_ASSERT(B6_PRECOND, condition)
-#define b6_postcond(condition)	__B6_ASSERT(B6_POSTCOND, condition)
+#define b6_check(condition)	__B6_CHECK(B6_CHECK, condition)
+#ifndef NDEBUG
+#define b6_assert(condition)	__B6_CHECK(B6_ASSERT, condition)
+#define b6_precond(condition)	__B6_CHECK(B6_PRECOND, condition)
+#define b6_postcond(condition)	__B6_CHECK(B6_POSTCOND, condition)
+#else /*NDEBUG*/
+#define b6_assert(condition) (void)(condition)
+#define b6_precond(condition) (void)(condition)
+#define b6_postcond(condition) (void)(condition)
+#endif /*NDEBUG*/
 
 #endif /*ASSERT_H_*/
